ef237c6b9201e32afed038bcefeb52f818b02e01
Charles Demers <charlesantoinedemers@hotmail.com>
2014-09-22 08:27:40 -0400

    First commit

---
+++ includes/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo (isset($title)) ? $title . ' | WebsiteName' : 'WebsiteName' ?></title>
+
+	<!-- Styling -->	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+	<!-- Javascript -->
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+	require_once 'functions/init.php';
+
+	$title = '';
+	require_once 'includes/head.php';
+?>
+<!-- Content starts -->
+
+<!-- Content ends -->
+<?php 
+	require_once 'includes/foot.php';
+?>
