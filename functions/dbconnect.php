bb4ab81693f52a6a44f990c265da1ae1571b82f2
Charles Demers <charlesantoinedemers@hotmail.com>
2014-09-22 08:33:57 -0400

    Added the database connection

---
+++ functions/dbconnect.php
@@ -0,0 +1,13 @@
+<?php 
+
+	try
+	{
+		$pdo = new PDO('mysql:host=localhost;dbname=test;charset=utf-8', 'root', '');
+	}
+	catch (PDOException $e)
+	{
+		echo 'An error occured while connecting to the database.';
+		// some_logging_function($e->getMessage());
+	}
+
+?>
