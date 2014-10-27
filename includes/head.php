ce29525ab7f14a245a5b3aa478ee56f8a9d8bdb9
Charles Demers <charlesantoinedemers@hotmail.com>
2014-10-27 07:14:06 -0400

    No more bootstrap - jQuery 2+

--- includes/head.php
+++ includes/head.php
@@ -3,13 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php echo (isset($title)) ? $title . ' | WebsiteName' : 'WebsiteName' ?></title>
-
-	<!-- Styling -->	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-
-	<!-- Javascript -->
-	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/main.css">	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 </head>
 <body>
